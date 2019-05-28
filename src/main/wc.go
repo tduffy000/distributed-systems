8c4b8a321e81b0110053b55f152224383778e031
Thomas Duffy <tduffy000@citymail.cuny.edu>
2019-05-28 18:08:42 -0400

    complete implementations of map & reduce

--- src/main/wc.go
+++ src/main/wc.go
@@ -1,18 +1,53 @@
 package main
 
-import "os"
-import "fmt"
-import "mapreduce"
-import "container/list"
+import (
+  "os"
+  "fmt"
+  "mapreduce"
+  "container/list"
+  "strings"
+  "unicode"
+  "strconv"
+)
 
 // our simplified version of MapReduce does not supply a
 // key to the Map function, as in the paper; only a value,
 // which is a part of the input file contents
 func Map(value string) *list.List {
+  // KeyValue container
+  kvs := list.New()
+
+  // white-space boolean sub-routine
+  split := func(char rune) bool {
+    return !unicode.IsLetter(char)
+  }
+
+  // apply to split along white-space
+  s := strings.FieldsFunc(value, split)
+
+  // iterate over each word & transform to (k ,v) pair
+  // f: word => (word, 1)
+  for _, el := range s {
+    kv := mapreduce.KeyValue{el, "1"}
+    kvs.PushBack(kv)
+  }
+
+  return kvs
 }
 
 // iterate over list and add values
 func Reduce(key string, values *list.List) string {
+  // null count
+  count := 0
+
+  // iterate over KeyValue pairs & reduce to a sum (counts)
+  for e := values.Front(); e != nil; e = e.Next() {
+    s := e.Value.(string)
+    i, _ := strconv.Atoi(s)
+    count += i
+  }
+
+  return strconv.Itoa(count)
 }
 
 // Can be run in 3 ways:
@@ -26,7 +61,7 @@ func main() {
     if os.Args[3] == "sequential" {
       mapreduce.RunSingle(5, 3, os.Args[2], Map, Reduce)
     } else {
-      mr := mapreduce.MakeMapReduce(5, 3, os.Args[2], os.Args[3])    
+      mr := mapreduce.MakeMapReduce(5, 3, os.Args[2], os.Args[3])
       // Wait until MR is done
       <- mr.DoneChannel
     }
--- src/mapreduce/mapreduce.go
+++ src/mapreduce/mapreduce.go
@@ -61,7 +61,7 @@ type MapReduce struct {
   stats *list.List
 
   // Map of registered workers that you need to keep up to date
-  Workers map[string]*WorkerInfo 
+  Workers map[string]*WorkerInfo
 
   // add any additional state here
 }
@@ -257,7 +257,7 @@ func DoReduce(job int, fileName string, nmap int,
       _, ok := kvs[kv.Key]
       if !ok {
         kvs[kv.Key] = list.New()
-      } 
+      }
       kvs[kv.Key].PushBack(kv.Value)
     }
     file.Close()
